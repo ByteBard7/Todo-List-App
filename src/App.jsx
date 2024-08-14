d4d0c69114741e00a2979adbea3caf6876ce65f0
Saurabh Varunkar <128482646+ByteBard7@users.noreply.github.com>
2024-08-14 13:50:05 +0530

    Added functionality to store todos in an array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 const App = () => {
   const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: crypto.randomUUID(), title: newTodo }];
+    });
   };
 
   const handleInput = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     setNewTodo(e.target.value);
   };
 
@@ -16,7 +20,7 @@ const App = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" value={newTodo} onChange={handleInput} />
-        <button>Add</button>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
