09c4708863cff9aba3a29b4e389719253c4ee976
Saurabh Varunkar <128482646+ByteBard7@users.noreply.github.com>
2024-08-14 13:30:59 +0530

    Added Input form for new todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setNewTodo(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newTodo} onChange={handleInput} />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
